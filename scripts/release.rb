9526281180dcca796e5bb9893e2e76a880304e49
Matt Haynes <matt@matthaynes.net>
2011-08-31 13:36:46 +0100

    Add release

---
+++ scripts/release.rb
@@ -0,0 +1,10 @@
+temp = "../temp"
+puts `git clone -l -s -b gh-pages . #{temp}`
+if system("mvn release:perform")
+    puts `cd #{temp} && git add -A && git commit -m "releasing artifacts" && git push origin gh-pages`
+    puts `git push origin gh-pages`
+    puts " artifact released successfully "
+else
+  puts " could not release artifact "
+end
+puts `rm -r #{temp}`
