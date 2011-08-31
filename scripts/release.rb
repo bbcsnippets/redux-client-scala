0654dc726bcf0248b52496bd473d5ab591977bfb
Matt Haynes <matt@matthaynes.net>
2011-08-31 14:12:24 +0100

    Update release

--- scripts/release.rb
+++ scripts/release.rb
@@ -1,15 +1,14 @@
 temp = "../temp"
 puts `git clone -l -s -b gh-pages . #{temp}`
 if system("mvn release:perform")
-    puts `cd #{temp}`
-    puts `for DIR in $(find . -type d); do
+    puts `for DIR in $(find #{temp} -type d); do
       (
         echo -e "<html>\\n<body>\\n<h1>Directory listing</h1>\\n<hr/>\\n<pre>"
         ls -1pa "${DIR}" | grep -v "^\\./$" | grep -v "^index\\.html$" | awk '{ printf "<a href=\\"%s\\">%s</a>\\n",$1,$1 }'
         echo -e "</pre>\\n</body>\\n</html>"
       ) > "${DIR}/index.html"
     done`
-    puts `git add -A && git commit -m "releasing artifacts" && git push origin gh-pages`
+    puts `cd #{temp} && git add -A && git commit -m "releasing artifacts" && git push origin gh-pages`
     puts `git push origin gh-pages`
     puts " artifact released successfully "
 else
